cfa2be31cdd2b68a069b184294275992d872db29
Student <student@Admins-MacBook-Pro.local>
2024-11-20 16:58:56 -0500

    Draft of Horoscope Game.

---
+++ Horoscope_Game.py
@@ -0,0 +1,27 @@
+print("Welcome to horoscope! Type your birth sign to receive your horoscope. :) ")
+player_Answer = input("Type your response here: ")
+
+if player_Answer == ("Aries" or "aries"):
+    print("A subject of interest that you've been pursuing could bring up some deeply buried emotions today.")
+elif player_Answer == ("Taurus" or"taurus"):
+    print("This could be a confusing day for you, Taurus. Financial issues might be complicated by computer glitches or failures in communication.")
+elif player_Answer == ("Gemini" or "gemini"):
+    print("Financial issues might have you and your partner a bit more on edge than you, Gemini. Perhaps money is tighter than usual right now, and you have to put off purchases you want to make. Try to work it out.")
+elif player_Answer == ("Cancer" or "cancer"):
+    print("Today your intellect is flying high, Cancer. A number of new ideas, perhaps about political or social issues, might excite your curisosity and cause you to want to spend hours in the library or on the internet.")
+elif player_Answer == ("Leo" or "leo"):
+    print("Your imagination is working overtime today, Leo, and you might spend hours at a computer writing or in front of an easel or piano creating your own band of painting or music. This drive to create comes from the heart and shouldn't be ignored no matter what other issues come up.")
+elif player_Answer == ("Virgo" or "virgo"):
+    print("A conflict between your responsibilities to family and obligations to freinds could rear its ugly head today, Virgo. You might want to attend an event but need to deal with emotional issues in the home immediately.")
+elif player_Answer == ("Libra" or "libra"):
+    print("Dissatisfaction with certain job conditions might come to a head today, Libra. A discussion with colleagues might be necessary to defuse a potentially explosive situation. Clear, honest communincation is necessary in order to avoid misunderstandings.")
+elif player_Answer == ("Scorpio" or "scorpio"):
+    print("If you've been planning a long trip by air, Scorpio, today you might doubt whether you should continue with your plans. Money or safety might be the issues.")
+elif player_Answer == ("Sagittarius" or "sagittarius"):
+    print("Today you might feel too overwrought to concentrate on whatever goals you're trying to reach, particulary if they're financial.")
+elif player_Answer == ("Capricorn" or "capricorn"):
+    print("You tend to be pretty self-assured, Capricorn, but seslf-esteem issues daitng from long ago could surface today. You might feel like schoolchild called on to recite.")
+elif player_Answer == ("Aquarius" or "aquarius"):
+    print("Today you might feel a bit bloated and slow, Aquarius, which could interfere with your daily routine. You could also feel unmotivated about work.")
+elif player_Answer == ("Pisces" or "pisces"):
+    print("Today your job or another activity could require your artistic skills for a particular project, Pisces.")
